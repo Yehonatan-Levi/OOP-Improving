6a6f4b1288d0158b93ea9dbfc22c30d2a7152cc4
Yehonatan-Levi <yehonatan.levi0904@gmail.com>
2026-08-22 15:20:58 +0300

    Initial commit
    (cherry picked from commit 04c69acee06e31d2478d9c157389355d0c855430)

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,5 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-
-
-import customers.*;
-import shop.Drinks;
-import shop.Order;
-
 void main() {
-    GenericCustomer customer = new RegularCustomer("Jonathan", PaymentMethod.GIFT_CARD);
-    Order order = new Order(customer);
-
-    order.addDrink(Drinks.COFFEE);
-    order.addDrink(Drinks.COFFEE);
-    order.addDrink(Drinks.COFFEE);
-
-
-    System.out.println(order.summary());
 
 }
