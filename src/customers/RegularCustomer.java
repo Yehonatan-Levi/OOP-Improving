15f6881fee28b995435d031e26d9ee8121fe3f4b
Yehonatan-Levi <yehonatan.levi0904@gmail.com>
2026-09-05 12:43:52 +0300

    restarted master, master will be a blank project and every branch will be an improving session

--- src/customers/RegularCustomer.java
+++
@@ -1,9 +0,0 @@
-package customers;
-
-import discount.DiscountConstants;
-
-public class RegularCustomer extends GenericCustomer{
-    public RegularCustomer(String name, PaymentMethod paymentMethod) {
-        super(name, paymentMethod);
-    }
-}
--- src/interfaces/IMenuItem.java
+++
@@ -1,5 +0,0 @@
-package interfaces;
-
-public interface IMenuItem {
-    double getPrice();
-}
